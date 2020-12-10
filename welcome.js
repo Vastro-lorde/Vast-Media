49f79316bdb2ae0b747cc9f2117700a1c0c8fdfc
Seun Daniel Omatsola <omatsolaseund@gmail.com>
2020-12-10 11:44:20 +0100

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,5 @@
+var sendmessage = prompt ("Welcome to Vast Media, please send a message");
+sendmessage.lenght < 50;
+if (sendmessage.length = true) alert("Thank You for your feedbaack");
+else alert("word count exceeded");
+alert ("This is your message (" +sendmessage.toUpperCase(sendmessage.slice(0,15)) + ")");
